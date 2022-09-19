1c8676730d8ec644d069f38bc54e585f44c81fa9
mark <markferdinan.mfa@gmail.com>
2022-09-19 23:35:26 +0800

    change cors header

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.use(express.json())
 app.use((req,res ,next ) => {
     console.log(req.path, req.method)
     // update to match the domain you will make the request from
-    res.header("Access-Control-Allow-Origin", "https://profound-vacherin-b55afb.netlify.app/")
+    res.header("Access-Control-Allow-Origin", "*")
     res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT")
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
     next()
