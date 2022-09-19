5c12454aab1a114b16c10dfc4ceac074169061f1
mark <markferdinan.mfa@gmail.com>
2022-09-19 23:42:46 +0800

    change cors header

--- server.js
+++ server.js
@@ -12,10 +12,6 @@ app.use(express.json(), cors())
 
 app.use((req,res ,next ) => {
     console.log(req.path, req.method)
-    // update to match the domain you will make the request from
-    res.header("Access-Control-Allow-Origin", "*")
-    res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT")
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
     next()
 })
 
